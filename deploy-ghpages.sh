7b07685c99b816482ac63e9c3ecc03f3c716faa9
barriquello <barriquello@gmail.com>
2016-02-15 12:15:53 -0200

    update travis

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 echo -e "Publishing doxygen. \n"
-HTML_PATH=build-doc
-rm -rf ${HTML_PATH}
-mkdir -p ${HTML_PATH}
-git clone -b gh-pages "${GH_REF}" --single-branch ${HTML_PATH}
-cd ${HTML_PATH}
-git rm -rf .	
-cp -R ../doc/html ./
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git add .
-git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-echo -e "Published doxygen to gh-pages. \n"
+set -e
+
+if [ -n "$GH_TOKEN" ]; then
+	cd docs/html
+	git init
+	git config --global user.email "travis@travis-ci.org"
+	git config --global user.name "travis-ci"
+	git add *
+	git commit -m "Deployed to Github Pages"
+	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+	echo -e "Published doxygen to gh-pages. \n"
+fi
 
 
