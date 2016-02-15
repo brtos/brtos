0a850629c4aa467ac587839f4b13f2dc095fb62d
barriquello <barriquello@gmail.com>
2016-02-15 13:03:56 -0200

    update travis

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo -e "Publishing doxygen. \n"
-
+echo "Publishing doxygen. \n"
 if [ -n "$GH_TOKEN" ]; then
 	cd docs/html
 	git init
@@ -8,8 +7,8 @@ if [ -n "$GH_TOKEN" ]; then
 	git config --global user.name "travis-ci"
 	git add *
 	git commit -m "Deployed to Github Pages"
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-	echo -e "Published doxygen to gh-pages. \n"
+	git push --force --quiet "https://${GH_TOKEN}@github.com/brtos/brtos.git" master:gh-pages
+	echo "Published doxygen to gh-pages. \n"
 fi
 
 
