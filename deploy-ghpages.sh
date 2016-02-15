ce41dacf91e57d5525f1619e242e466db68fd888
barriquello <barriquello@gmail.com>
2016-02-15 11:48:31 -0200

    update travis

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 echo -e "Publishing doxygen. \n"
+rm -rf ./out || exit 0;
+mkdir out; 
+cd out	
+cp -R $PROJ_ROOT/doc/html ./html
 
-rm -rf $HOME/out || exit 0;
-mkdir $HOME/out; 
-( 	cd $HOME/out	
-	cp -R $PROJ_ROOT/doc/html ./html
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "travis-ci"
-	git add .
-	git commit -m "Deployed to Github Pages"
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+echo -e "Published doxygen to gh-pages. \n"
 
-	echo -e "Published doxygen to gh-pages. \n"
-)
 
