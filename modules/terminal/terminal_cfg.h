4469d1d7bd03ac4c0986643b393857decc641801
barriquello <barriquello@gmail.com>
2016-10-05 13:15:21 -0300

    adiciona comando p/ repetir comando anterior

--- modules/terminal/terminal_cfg.h
+++ modules/terminal/terminal_cfg.h
@@ -1,6 +1,8 @@
 /*
  * terminal_cfg.h
  *
+ *  Created on: 28/04/2016
+ *      Author: Universidade Federal
  */
 
 #ifndef APP_TERMINAL_CFG_H_
@@ -8,18 +10,18 @@
 
 /************* TERMINAL CONFIG *************************/
 #define TERM_INPUT_BUFSIZE 		32
-#define UP_KEY_CHAR				(char)'\033'
-#define CHARS_TO_DISCARD		2
+#define UP_KEY_CHAR				(char)-32
+#define CHARS_TO_DISCARD		1
 
 /* Supported commands */
 /* Must be listed in alphabetical order !!!! */
 
 /*  ------ NAME ------- HELP --- */
 #define COMMAND_TABLE(ENTRY) \
-ENTRY(help,"Help Command")     \
-ENTRY(runst, "Runtime Stats")\
-ENTRY(top,"System info")    \
-ENTRY(ver,"System version")
+	ENTRY(help,"Help Command")     \
+	ENTRY(runst,"Running stats")	\
+	ENTRY(top,"System info")    \
+	ENTRY(ver,"System version")
 
 #define HELP_DESCRIPTION         1
 
@@ -29,7 +31,6 @@ ENTRY(ver,"System version")
 #else
 #include "printf_lib.h"
 #define TERM_PRINT(...) printf_lib(__VA_ARGS__);
-#define CUSTOM_PRINTF	1
 #endif
 
 
