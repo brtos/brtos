7855cc06ac304fe6f5b00b018c97b15e83cfee9f
barriquello <barriquello@gmail.com>
2016-10-29 15:53:50 -0200

    altera terminal p/ corrigir conflito

--- modules/terminal/terminal.c
+++ modules/terminal/terminal.c
@@ -45,7 +45,6 @@ void terminal_init(char (*_putchar_func)(char))
 	#if defined(TERM_PRINT) && defined(CUSTOM_PRINTF)
 	printf_install_putchar(_putchar_func);
 	#endif
-	term_putchar_install(_putchar_func);
 	putchar_func('\n');
 	putchar_func('\r');
 	putchar_func('>');
